662219ff15cf36182db1d8186238ca06cc47c5d6
Richard Foster <richard@madetech.co.uk>
2015-10-04 00:29:58 +0100

    Add HomePage component

---
+++ app/components/pages/home-page.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <div>hello!</div>
+    );
+  }
+}
+
+module.exports = HomePage;
--- app/routes.jsx
+++ app/routes.jsx
@@ -2,10 +2,12 @@ const React = require('react');
 const { Route, Link } = require('react-router');
 
 const BaseLayout = require('./components/layouts/base');
+const HomePage = require('./components/pages/home-page');
 const ArticlePage = require('./components/pages/article-page');
 
 module.exports = (
-  <Route name="homepage" path="/" handler={BaseLayout}>
+  <Route path="/" handler={BaseLayout}>
+    <Route name="homepage" path="/" handler={HomePage} />
     <Route name="sample-article" path="sample" handler={ArticlePage} />
   </Route>
 );
